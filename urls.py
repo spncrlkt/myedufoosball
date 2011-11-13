e8764cbd36e7d4722098ed93fbe6807f16918990
Spencer Liechty <spencerliechty@gmail.com>
2011-11-13 15:50:18 -0600

    Add git ignore update TODO

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
+## Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
